533890664c5e49d89e09d34bd4e288b9abcec18d
Hendrik Schwind-Hansen <info@hendrik-sh.de>
2024-02-23 22:40:40 +0100

    feat: implemented teaser

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const menu = document.querySelector("#menu");
 const mobileNav = document.querySelector("#mobile-nav");
+const teaserBtn = document.querySelector("#teaser-btn");
+const teaserLine = document.querySelector("#teaser-line");
+const teaserList = document.querySelector("#teaser-list");
+const teaserBanner = document.querySelector("#teaser-banner");
+const teaserTextP = document.querySelector("#teaser-text-p");
 
 menu.addEventListener("click", () => {
   mobileNav.classList.toggle("show");
@@ -9,4 +14,29 @@ mobileNav.addEventListener("click", (event) => {
   if (event.target.tagName === "A") {
     mobileNav.classList.toggle("show");
   }
+});
+
+teaserBtn.addEventListener("click", () => {
+  teaserTextP.textContent = "";
+  teaserBanner.classList.remove("display");
+  let count = 59;
+  teaserBtn.textContent = count;
+  teaserList.classList.add("display");
+  let interval = setInterval(() => {
+    console.log(count);
+    teaserBtn.textContent = count;
+    if (count > 0) {
+      count--;
+    }
+    if (count === 0) {
+      teaserBtn.textContent = "Start";
+      teaserLine.classList.remove("animate");
+      teaserBtn.classList.remove("move-down");
+      teaserList.classList.remove("display");
+      teaserBanner.classList.add("display");
+      clearInterval(interval);
+    }
+  }, 1000);
+  teaserLine.classList.add("animate");
+  teaserBtn.classList.add("move-down");
 });
